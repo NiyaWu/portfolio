df5dffbdd6402abfd38a4eeafe00669643b717a8
Corner118 <twmniya@gmail.com>
2020-08-02 15:07:00 +0800

    Init

---
+++ docs/precache-manifest.75d85335b4720634fe5b3e63bfd6a236.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa451b9aab91cad01d5e57355719b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67d87fb13d1db473e194",
+    "url": "/static/css/main.a071eb1e.chunk.css"
+  },
+  {
+    "revision": "f4dc9c4ca1d6933fbe0b",
+    "url": "/static/js/2.77f8d451.chunk.js"
+  },
+  {
+    "revision": "67d87fb13d1db473e194",
+    "url": "/static/js/main.57929cb6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fc603c08d955eb79271f29b36abad07",
+    "url": "/static/media/01.7fc603c0.jpg"
+  },
+  {
+    "revision": "8b31747c21e4c96759f126fb17ad1c63",
+    "url": "/static/media/02.8b31747c.jpg"
+  },
+  {
+    "revision": "5a0296930b8eed2fe0b6cc9c8541693a",
+    "url": "/static/media/3myLogin.5a029693.png"
+  },
+  {
+    "revision": "0751e7cf2e43abe1c409105f27a0c969",
+    "url": "/static/media/AnalyticFlow.0751e7cf.png"
+  },
+  {
+    "revision": "68d65428dcaba1253e69eda98ccb2bdc",
+    "url": "/static/media/AnalyzingTool.68d65428.png"
+  },
+  {
+    "revision": "98574288fb98adfa104b1132f62f2c62",
+    "url": "/static/media/Apad.98574288.png"
+  },
+  {
+    "revision": "506d6128b9b817d52d914839e6fd4528",
+    "url": "/static/media/BAPFlow.506d6128.png"
+  },
+  {
+    "revision": "f616e6f95f468631dbecb85c7c694436",
+    "url": "/static/media/BG-other.f616e6f9.jpg"
+  },
+  {
+    "revision": "9516ca8a8369dd92978eaf05afc25ff1",
+    "url": "/static/media/CustomerService.9516ca8a.png"
+  },
+  {
+    "revision": "d04cbea032dbd63ee0b0233242080039",
+    "url": "/static/media/DEV.d04cbea0.png"
+  },
+  {
+    "revision": "663d978c1bcf0919bc0e33523fcbf7ce",
+    "url": "/static/media/Dashboard-BehaviorAnalytics.663d978c.png"
+  },
+  {
+    "revision": "0e43673fa0289d2b31e2b8951d81650d",
+    "url": "/static/media/Dashboard-BusinessIntelligence.0e43673f.png"
+  },
+  {
+    "revision": "7b3d2d09772f7661de689abad5d98c24",
+    "url": "/static/media/Dashboard-PeopleAnalytics.7b3d2d09.png"
+  },
+  {
+    "revision": "afcde30bfcc98fcb3cd0539fbe4a0aa0",
+    "url": "/static/media/Dashboard-Status.afcde30b.png"
+  },
+  {
+    "revision": "d6909a41b12e584150222cf06f36b49a",
+    "url": "/static/media/Dashboard-VehicleAnalytics.d6909a41.png"
+  },
+  {
+    "revision": "3ff6d246340871a9ebf5fafb3d8f9b40",
+    "url": "/static/media/Event-Dashboard.3ff6d246.png"
+  },
+  {
+    "revision": "e25f6e04d0760eabaf00ff724afc211e",
+    "url": "/static/media/IVAR-BAP-Title.e25f6e04.png"
+  },
+  {
+    "revision": "1acb9d172f97cea9bc15b7081e66d775",
+    "url": "/static/media/IVAR-in.1acb9d17.gif"
+  },
+  {
+    "revision": "5b8d1eed88b4ef6a2233bfa0ef2cd0f8",
+    "url": "/static/media/IVAR2Flow.5b8d1eed.png"
+  },
+  {
+    "revision": "fe0bdb1969ca9cc5855a938e98cb2359",
+    "url": "/static/media/Ivar2-dark.fe0bdb19.png"
+  },
+  {
+    "revision": "4834596defc7225a76dac97f7167bc38",
+    "url": "/static/media/Ivar2-light.4834596d.png"
+  },
+  {
+    "revision": "5b9b5bf1fa169499395b852750b29bd3",
+    "url": "/static/media/IvarAnalytic.5b9b5bf1.png"
+  },
+  {
+    "revision": "ca4b48b9c5b85ae5b3576804eec1e2ac",
+    "url": "/static/media/IvarDashboard.ca4b48b9.png"
+  },
+  {
+    "revision": "918ff5b14cb81563d5b7d398e4a33194",
+    "url": "/static/media/Mockupflow.918ff5b1.png"
+  },
+  {
+    "revision": "54751fc0dcdffbae20efa678e4739f51",
+    "url": "/static/media/OneVideo.54751fc0.png"
+  },
+  {
+    "revision": "32b6592dd4d15ac88b35c56baf4efa7f",
+    "url": "/static/media/SEflow.32b6592d.png"
+  },
+  {
+    "revision": "297cd1aa5109f1af360fce1f1d188e5c",
+    "url": "/static/media/Schedule01.297cd1aa.jpg"
+  },
+  {
+    "revision": "2918384c2971c948e1ba78360d6a73da",
+    "url": "/static/media/SourceExtract-flow.2918384c.png"
+  },
+  {
+    "revision": "e43b2b1ceb9e9bb057f39395a134a2cc",
+    "url": "/static/media/SourceExtratorTitle.e43b2b1c.png"
+  },
+  {
+    "revision": "aafaaf123e02d5d2cb3e80a5f9f4e7c7",
+    "url": "/static/media/StepOne.aafaaf12.png"
+  },
+  {
+    "revision": "982430711f4b0133ee88fc32b68a3698",
+    "url": "/static/media/StepTwo.98243071.png"
+  },
+  {
+    "revision": "3fd568749df95075b0d909926ad9683b",
+    "url": "/static/media/cctv-camera.3fd56874.jpg"
+  },
+  {
+    "revision": "d5b62970a5cee4c7d1e017608b114f71",
+    "url": "/static/media/chromecast-flow-phaseTwo.d5b62970.png"
+  },
+  {
+    "revision": "c5598e9072186957d3aa234f5b8f6ed0",
+    "url": "/static/media/chromecast-flow.c5598e90.png"
+  },
+  {
+    "revision": "d7a5995cae7a92296b4c4b14789c94a1",
+    "url": "/static/media/iphone.d7a5995c.png"
+  },
+  {
+    "revision": "8a554ced412d1dd22083984afc211802",
+    "url": "/static/media/ivar2-deviceList.8a554ced.png"
+  },
+  {
+    "revision": "d77b6e784b05bbb8a091f9c898ac80d5",
+    "url": "/static/media/ivar2-ivarList.d77b6e78.png"
+  },
+  {
+    "revision": "4b91f31b86f2e7bc0646d62f61d4411d",
+    "url": "/static/media/ivar2WF.4b91f31b.png"
+  },
+  {
+    "revision": "5cbb4aa059515ea9b3c5fd2717ee17ff",
+    "url": "/static/media/ivarRedesign.5cbb4aa0.png"
+  },
+  {
+    "revision": "fc71c5ec85bd9701ec9933c235347901",
+    "url": "/static/media/mockup.fc71c5ec.png"
+  },
+  {
+    "revision": "933c9a19f0d2d846d4e96b5aab85d5bd",
+    "url": "/static/media/monthPay.933c9a19.png"
+  },
+  {
+    "revision": "a1de0515afeb3f3334f265be16d2b6c9",
+    "url": "/static/media/myPhoto.a1de0515.jpg"
+  },
+  {
+    "revision": "94542f3aabf2950714752a122e6053a4",
+    "url": "/static/media/myVideo-Chromecat-Title.94542f3a.png"
+  },
+  {
+    "revision": "2d08e6effcce1261caa9d634cdcbd0ed",
+    "url": "/static/media/oldFlow.2d08e6ef.png"
+  },
+  {
+    "revision": "1e9bc8e049be616000c8553926f19670",
+    "url": "/static/media/test01.1e9bc8e0.png"
+  },
+  {
+    "revision": "c09737dd7907724346c4b4e8388d2498",
+    "url": "/static/media/test02.c09737dd.png"
+  },
+  {
+    "revision": "208d69f45882777a51cf4dc371373ea9",
+    "url": "/static/media/test03.208d69f4.png"
+  },
+  {
+    "revision": "918bbd886a2c3be539ce954d2cfb0bba",
+    "url": "/static/media/test04.918bbd88.png"
+  },
+  {
+    "revision": "4cdd1e4c38363f13db538e8c89bc212e",
+    "url": "/static/media/test05.4cdd1e4c.png"
+  },
+  {
+    "revision": "73857bd8cbe66f396dbe1687e2546200",
+    "url": "/static/media/testA.73857bd8.jpg"
+  },
+  {
+    "revision": "a9e2259897772851acf1717dc7d793da",
+    "url": "/static/media/testB.a9e22598.gif"
+  },
+  {
+    "revision": "b8d137a707de47664c18997f707d6457",
+    "url": "/static/media/testC.b8d137a7.jpg"
+  },
+  {
+    "revision": "2ca1b501baf542e780e808e286487635",
+    "url": "/static/media/testD.2ca1b501.jpg"
+  },
+  {
+    "revision": "c49ec9867d455053ead454ca285e6fa6",
+    "url": "/static/media/turnLeftRole.c49ec986.png"
+  },
+  {
+    "revision": "eb7666ce8b41bbd9d5c340f6a908234c",
+    "url": "/static/media/userMessage01.eb7666ce.png"
+  },
+  {
+    "revision": "46055c4cae19530fbc88677fb2376348",
+    "url": "/static/media/video.46055c4c.jpg"
+  },
+  {
+    "revision": "6b9b37ccd43e41c0dd53310c6234c3f5",
+    "url": "/static/media/video3myTitle.6b9b37cc.png"
+  },
+  {
+    "revision": "0553d7da3ea1169289bb9101924a8b81",
+    "url": "/static/media/videoMockup1.0553d7da.png"
+  },
+  {
+    "revision": "7d4469d1251302474f7792c67cbd7a71",
+    "url": "/static/media/videoMockup2.7d4469d1.png"
+  },
+  {
+    "revision": "ee3390e735ccab5231bea6fbb1532f6b",
+    "url": "/static/media/videoMockup3.ee3390e7.png"
+  },
+  {
+    "revision": "302a0e9b243d810fe3473e2cb1ddcf2c",
+    "url": "/static/media/videoRevision.302a0e9b.png"
+  }
+]);
