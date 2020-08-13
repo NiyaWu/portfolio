cdf25fb45485e3f0ef8e545d2a0cbd372aa09b41
Corner118 <twmniya@gmail.com>
2020-08-13 12:33:39 +0800

    img size

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6642b05199c04eb67f3b1f49b0d33fa3.js"
+  "/portfolio/precache-manifest.7132111397f64b7430e12dc562f52846.js"
 );
 
 self.addEventListener('message', (event) => {
