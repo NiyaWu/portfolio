81b0e1aa1d61c00486db990e240056c883cf7d91
Corner118 <twmniya@gmail.com>
2020-08-18 16:58:45 +0800

    text change

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.92cd70e29b372db38193c0a50b949e8f.js"
+  "/portfolio/precache-manifest.a47a26c72ca074145e21c6a4cd66a83c.js"
 );
 
 self.addEventListener('message', (event) => {
