43c398ec0b09a8a765c96b08c4feae64646a40a3
Corner118 <twmniya@gmail.com>
2020-09-01 16:55:38 +0800

    change domain

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7f3cc5115c4355dcc1407a12397bdc19.js"
+  "/portfolio/precache-manifest.6bc765739321de82f2b00a76fc0c96e8.js"
 );
 
 self.addEventListener('message', (event) => {
