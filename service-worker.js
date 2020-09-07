560f36c3525573633711871a89e8fdc31e092313
Corner118 <twmniya@gmail.com>
2020-09-07 12:01:42 +0800

    picture modify

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6bc765739321de82f2b00a76fc0c96e8.js"
+  "/portfolio/precache-manifest.59a7d2245171a56bd3d598d827408048.js"
 );
 
 self.addEventListener('message', (event) => {
