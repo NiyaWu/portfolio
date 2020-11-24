3ca5c0fdbd44c5636b600f6ab0fa5e1607c7e191
Corner118 <twmniya@gmail.com>
2020-11-24 11:43:41 +0800

    background change

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97fab7ca208c1509b46720597c7afac1.js"
+  "/precache-manifest.3ca37f6022074368182defe970ec0232.js"
 );
 
 self.addEventListener('message', (event) => {
